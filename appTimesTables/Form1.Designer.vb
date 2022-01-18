02229fa4ea24132168d87ea3cbae20053d78cd8b
20211374 <20211374@DFEI-S10-64>
2022-01-18 14:15:04 +0000

    Add project files.

---
+++ appTimesTables/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.txtStart = New System.Windows.Forms.TextBox()
+        Me.txtEnd = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lboDisplay = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(125, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Which times tables?"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(183, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Start at:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(186, 145)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "End at:"
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(261, 30)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumber.TabIndex = 3
+        '
+        'txtStart
+        '
+        Me.txtStart.Location = New System.Drawing.Point(261, 94)
+        Me.txtStart.Name = "txtStart"
+        Me.txtStart.Size = New System.Drawing.Size(100, 20)
+        Me.txtStart.TabIndex = 4
+        '
+        'txtEnd
+        '
+        Me.txtEnd.Location = New System.Drawing.Point(261, 145)
+        Me.txtEnd.Name = "txtEnd"
+        Me.txtEnd.Size = New System.Drawing.Size(100, 20)
+        Me.txtEnd.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(128, 215)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(233, 50)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Display Times table"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'lboDisplay
+        '
+        Me.lboDisplay.FormattingEnabled = True
+        Me.lboDisplay.Location = New System.Drawing.Point(128, 307)
+        Me.lboDisplay.Name = "lboDisplay"
+        Me.lboDisplay.Size = New System.Drawing.Size(233, 264)
+        Me.lboDisplay.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(506, 605)
+        Me.Controls.Add(Me.lboDisplay)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtEnd)
+        Me.Controls.Add(Me.txtStart)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "TimesTables"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNumber As TextBox
+    Friend WithEvents txtStart As TextBox
+    Friend WithEvents txtEnd As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lboDisplay As ListBox
+End Class
---
+++ appTimesTables/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intNumbers() As Integer
+        Dim intMultiply As Integer
+        Dim intStart As Integer
+        Dim intEnd As Integer
+
+
+
+        lboDisplay.Items.Clear()
+
+        intMultiply = Val(txtNumber.Text)
+        intEnd = Val(txtEnd.Text)
+        intStart = Val(txtStart.Text)
+
+        'Re dimensioning, giving a vairable a different tsize
+        ReDim intNumbers(intEnd)
+
+        For i = intStart To intEnd
+            intNumbers(i) = i * intMultiply
+            lboDisplay.Items.Add(intMultiply.ToString & " Times " & i & " = " & intNumbers(i))
+        Next
+
+
+
+
+    End Sub
+End Class
